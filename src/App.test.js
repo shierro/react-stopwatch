62eca6f6df9c3ff5581993444cac6c74144ba083
Theo <theodoro.pelingan@gmail.com>
2021-11-17 02:12:10 +0800

    :hammer: add test for stopping a stopwatch successfully

--- src/App.test.js
+++ src/App.test.js
@@ -13,11 +13,20 @@ test('Renders initial Stopwatch elements successfully', () => {
 
 test('Start Stopwatch successfully', () => {
   render(<App />);
-  const startButton = screen.getByText(/START/i);
-  const timeElement = screen.getByTestId('time');
-  fireEvent.click(startButton);
-  const stopButton = screen.getByText(/STOP/i);
-  expect(stopButton).toBeInTheDocument();
+  fireEvent.click(screen.getByText(/START/i));
+  expect(screen.getByText(/STOP/i)).toBeInTheDocument();
   expect(screen.queryByText(/START/i)).toBeNull();
-  expect(timeElement.TEXT_NODE).toBeGreaterThan(1);
+  expect(screen.getByTestId('time').TEXT_NODE).toBeGreaterThan(1);
 });
+
+test('Stop Stopwatch successfully', async () => {
+  render(<App />);
+  const STOP_AT = 2;
+  fireEvent.click(screen.getByText(/START/i));
+  await new Promise((resolve) => setTimeout(resolve, STOP_AT * 1000));
+  fireEvent.click(screen.getByText(/STOP/i));
+  expect(screen.getByTestId('time').TEXT_NODE).toBe(STOP_AT + 1);
+  expect(screen.queryByText(/STOP/i)).toBeNull();
+  expect(screen.queryByText(/START/i)).toBeInTheDocument();
+});
+
